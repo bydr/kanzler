134642344b762880c1aa4d9a0a9e1233554354af
Daniil <zeveres88@gmail.com>
2020-06-23 14:14:15 +0300

    commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php require_once('head.php');?>
+
+<div class="link-list"
+     style="
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 20px;">
+  <a href="page_gid_style.php">Гид по стилю</a>
+  <a href="page_post.php">Страница статьи</a>
+  <a href="page_archive.php">Страница Архив статей</a>
+  <a href="page_card.php">Страница Карт</a>
+  <a href="page_payment_error.html">Страница ошибки оплаты</a>
+  <a href="page_payment_success.html">Страница успешной оплаты</a>
+</div>
+
+
+<?php require_once('foot.php');?>
